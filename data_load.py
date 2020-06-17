599d5c831aeef223c556e1f00bd24e65e541d9ec
Lingchen Zhu <zhulingchen@gmail.com>
2020-06-17 00:20:01 -0400

    finished part 1 and part 2

--- data_load.py
+++ data_load.py
@@ -34,9 +34,9 @@ class FacialKeypointsDataset(Dataset):
         
         # if image has an alpha color channel, get rid of it
         if(image.shape[2] == 4):
-            image = image[:,:,0:3]
+            image = image[:, :, :3]
         
-        key_pts = self.key_pts_frame.iloc[idx, 1:].as_matrix()
+        key_pts = self.key_pts_frame.iloc[idx, 1:].values
         key_pts = key_pts.astype('float').reshape(-1, 2)
         sample = {'image': image, 'keypoints': key_pts}
 
@@ -144,6 +144,10 @@ class RandomCrop(object):
 class ToTensor(object):
     """Convert ndarrays in sample to Tensors."""
 
+    def __init__(self, device, dtype=torch.cuda.FloatTensor):
+        self.device = device
+        self.dtype = dtype
+
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
          
@@ -157,5 +161,5 @@ class ToTensor(object):
         # torch image: C X H X W
         image = image.transpose((2, 0, 1))
         
-        return {'image': torch.from_numpy(image),
-                'keypoints': torch.from_numpy(key_pts)}
+        return {'image': torch.from_numpy(image).type(self.dtype).to(self.device),
+                'keypoints': torch.from_numpy(key_pts).type(self.dtype).to(self.device)}
--- models.py
+++ models.py
@@ -20,18 +20,32 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 5, stride=2)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
-
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=2)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, 3, stride=2)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.linear1 = nn.Linear(256, 400)
+        self.bn5 = nn.BatchNorm1d(400)
+        self.linear2 = nn.Linear(400, 136)
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.adaptive_avg_pool2d(x, (1, 1)).squeeze()  # global average pooling
+        x = F.relu(self.bn5(self.linear1(x)))
+        x = F.tanh(self.linear2(x))
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
